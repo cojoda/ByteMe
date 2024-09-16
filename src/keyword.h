aaec1dc2297f224c2167c137bc11342fee4506d9
Cody Danielson <cdanie10@uwyo.edu>
2024-09-16 09:55:43 -0600

    whitespace cleanup

--- src/keyword.h
+++ src/keyword.h
@@ -1,37 +1,43 @@
 #ifndef KEYWORD_H
 #define KEYWORD_H
 
+
+
 #include <iostream>
 #include <unordered_map>
 #include <string>
 
 #include "token.h"
 
+
+
 // TODO: codomain type back to Token (by removing quotes from codomain).
 // I set it this way for debugging so I can print to std::cout
 // std::unordered_map<std::string, Token> keyword_map = {
 std::unordered_map<std::string, std::string> keyword_map = {
-    {"do", "K_DO"},
-    {"double", "K_DOUBLE"},
-    {"else", "K_ELSE"},
-    {"exit", "K_EXIT"},
-    {"function", "K_FUNCTION"},
-    {"if", "K_IF"},
-    {"integer", "K_INTEGER"},
-    {"print_double", "K_PRINT_DOUBLE"},
+    {"do",            "K_DO"},
+    {"double",        "K_DOUBLE"},
+    {"else",          "K_ELSE"},
+    {"exit",          "K_EXIT"},
+    {"function",      "K_FUNCTION"},
+    {"if",            "K_IF"},
+    {"integer",       "K_INTEGER"},
+    {"print_double",  "K_PRINT_DOUBLE"},
     {"print_integer", "K_PRINT_INTEGER"},
-    {"print_string", "K_PRINT_STRING"},
-    {"procedure", "K_PROCEDURE"},
-    {"program", "K_PROGRAM"},
-    {"read_double", "K_READ_DOUBLE"},
-    {"read_integer", "K_READ_INTEGER"},
-    {"read_string", "K_READ_STRING"},
-    {"return", "K_RETURN"},
-    {"string", "K_STRING"},
-    {"then", "K_THEN"},
-    {"until", "K_UNTIL"},
-    {"while", "K_WHILE"}
+    {"print_string",  "K_PRINT_STRING"},
+    {"procedure",     "K_PROCEDURE"},
+    {"program",       "K_PROGRAM"},
+    {"read_double",   "K_READ_DOUBLE"},
+    {"read_integer",  "K_READ_INTEGER"},
+    {"read_string",   "K_READ_STRING"},
+    {"return",        "K_RETURN"},
+    {"string",        "K_STRING"},
+    {"then",          "K_THEN"},
+    {"until",         "K_UNTIL"},
+    {"while",         "K_WHILE"}
 };
+
+
 
 // TODO: change back Token
 // Token keyword_to_token(const std::string& keyword)
@@ -49,5 +55,7 @@ std::string keyword_to_token(const std::string& keyword)
     // return IDENTIFIER
     return "IDENTIFIER";
 }
+
+
 
 #endif // KEYWORD_H
