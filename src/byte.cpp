dc1218e254dba16b545188d322ce0fdf6392be11
Cody Danielson <cdanie10@uwyo.edu>
2024-10-27 14:45:31 -0600

    adds debug flag

--- src/byte.cpp
+++ src/byte.cpp
@@ -2,13 +2,15 @@
 #include <iostream>
 
 
-
+#define YYDEBUG
 // yyFlexLexer* flexlexer;  // Declare the flexlexer instance (static pointer to heap allocation)
 int yyparse();  // Declare the parser function
+extern int yydebug;
 
 
 int main(int argc, char** argv) 
 {
+    yydebug = 0;
     // flexlexer = new yyFlexLexer();
     return yyparse();
     
