91091d6c27f5049f781ffa948ff4533a00232d79
Cody Danielson <cdanie10@uwyo.edu>
2024-10-31 00:06:35 -0600

    cleanup

--- src/morph.cpp
+++ src/morph.cpp
@@ -1,17 +1,15 @@
-// #include <FlexLexer.h>
 #include <iostream>
 
 
-#define YYDEBUG
-// yyFlexLexer* flexlexer;  // Declare the flexlexer instance (static pointer to heap allocation)
-int yyparse();  // Declare the parser function
+
 extern int yydebug;
+
+int yyparse();
+
 
 
 int main(int argc, char** argv) 
 {
     yydebug = 0;
-    // flexlexer = new yyFlexLexer();
     return yyparse();
-    
 }
